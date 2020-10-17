21a67a634ec4cef4704341adc0ba147cef8e17e5
Gil Orcilla <mathnasiumcos@gmail.com>
2020-10-16 18:58:19 -0600

    add highscores.js file

---
+++ highscores.js
@@ -0,0 +1,6 @@
+// highscore variables
+
+var initialsForm = document.querySelector("//#endregioninitials-form");
+var initialsInput = document.querySelector("#initials-text");
+var initialsList = document.querySelector("#initialsList");
+var initialsScores = [];
