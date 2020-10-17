26207edee918484d235393355fe1a0be4940a270
Gil Orcilla <mathnasiumcos@gmail.com>
2020-10-16 19:00:42 -0600

    initiate/started questions.js file

---
+++ questions.js
@@ -0,0 +1,15 @@
+const questions = [
+    {
+        question: "The interactive web pages and applications are consists of?",
+        answers: [
+            { text: "<HTML>"},
+            { text: "<CSS>"},
+            { text: "<Javascript>"},
+            { text: "<All of the Above>"}
+        ],
+        correct: "<All of the above>"
+
+    },
+    {
+        question: 
+    }
