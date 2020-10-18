d8905d65349f0bf11267a70cb191cc7ea8f9ca62
Gil Orcilla <mathnasiumcos@gmail.com>
2020-10-18 17:29:06 -0600

    update the file with more questions

--- questions.js
+++ questions.js
@@ -11,12 +11,40 @@ const questions = [
 
     },
     {
-        question: "What is the correct syntax for referring to an external script called geek.js?"
+        question: "What is the correct syntax for referring to an external script called geek.js?",
         answers: [
-            { text: "<script src= 'geek.js'>"}
-            { text: "<script href= 'geek.js'>"}
-            { text: "<script ref = 'geek.js'>"}
-            { text: "<script name= 'geek.js'>"}
+            { text: "<script src= 'geek.js'>"},
+            { text: "<script href= 'geek.js'>"},
+            { text: "<script ref = 'geek.js'>"},
+            { text: "<script name= 'geek.js'>"},
         ]
         correct: "<script src= 'geek.js'>"
-    }
+    },
+    {
+        question: "Which of the following is the correct syntax to display GeeksforGeeks in an alear box using JavaScript?",
+        answers: [
+            { text: "alertbox("GeeksforGeeks")"},
+            { text: "msg ("GeeksforGeeks")"},
+            { text: "msgbox("GeeksforGeeks")"},
+            { text: "alert("GeeksforGeeks")"},
+        ]
+    },
+    {
+        question: "Which of the following is ot reserved word in JavaScript?",
+        answer: [
+            { text: "interface"},
+            { text: "throws"},
+            { text: "program"},
+            { text: "short"},
+        ]
+    },
+    {
+        question: "What is the HTML tag under which one can write the JavaScript code?",
+        answer: [
+            { text: "<javascript>"},
+            { text: "<scripted>"},
+            { text: "<script>"},
+            { text: "<js>"},
+        ]
+    },
+    
