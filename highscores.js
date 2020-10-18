97ee13bc62b434bfe3d56763ab54a399d39d1acb
Gil Orcilla <mathnasiumcos@gmail.com>
2020-10-17 19:43:56 -0600

    provide more contents to the file

--- highscores.js
+++ highscores.js
@@ -1,6 +1,19 @@
 // highscore variables
 
-var initialsForm = document.querySelector("//#endregioninitials-form");
+var initialsForm = document.querySelector("#initials-form");
 var initialsInput = document.querySelector("#initials-text");
 var initialsList = document.querySelector("#initialsList");
 var initialsScores = [];
+init();
+function renderInitials() {
+  initialsList.innerHTML = "";
+  for (var i = 0; i < initialsScores.length; i++) {
+    var initial = initialsScores[i];
+
+    var li = document.createElement("li");
+    li.textContent = initial;
+    li.setAttribute("data-index", i);
+
+    initialsList.appendChild(li);
+  }
+}
