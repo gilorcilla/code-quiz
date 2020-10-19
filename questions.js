2c4db6854153b8fd77b869f2f898ac90729e65c2
Gil Orcilla <mathnasiumcos@gmail.com>
2020-10-18 18:06:35 -0600

    update the file

--- questions.js
+++ questions.js
@@ -1,50 +1,102 @@
 const questions = [
-    {
-        question: "The interactive web pages and applications are consists of?",
-        answers: [
-            { text: "<HTML>"},
-            { text: "<CSS>"},
-            { text: "<Javascript>"},
-            { text: "<All of the Above>"}
-        ],
-        correct: "<All of the above>"
-
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called geek.js?",
-        answers: [
-            { text: "<script src= 'geek.js'>"},
-            { text: "<script href= 'geek.js'>"},
-            { text: "<script ref = 'geek.js'>"},
-            { text: "<script name= 'geek.js'>"},
-        ]
-        correct: "<script src= 'geek.js'>"
-    },
-    {
-        question: "Which of the following is the correct syntax to display GeeksforGeeks in an alear box using JavaScript?",
-        answers: [
-            { text: "alertbox("GeeksforGeeks")"},
-            { text: "msg ("GeeksforGeeks")"},
-            { text: "msgbox("GeeksforGeeks")"},
-            { text: "alert("GeeksforGeeks")"},
-        ]
-    },
-    {
-        question: "Which of the following is ot reserved word in JavaScript?",
-        answer: [
-            { text: "interface"},
-            { text: "throws"},
-            { text: "program"},
-            { text: "short"},
-        ]
-    },
-    {
-        question: "What is the HTML tag under which one can write the JavaScript code?",
-        answer: [
-            { text: "<javascript>"},
-            { text: "<scripted>"},
-            { text: "<script>"},
-            { text: "<js>"},
-        ]
-    },
-    
+  {
+    question: "The interactive web pages and applications are consists of?",
+    answers: [
+      { text: "<HTML>" },
+      { text: "<CSS>" },
+      { text: "<Javascript>" },
+      { text: "<All of the Above>" },
+    ],
+    correct: "<All of the above>",
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called geek.js?",
+    answers: [
+      { text: "<script src= 'geek.js'>" },
+      { text: "<script href= 'geek.js'>" },
+      { text: "<script ref = 'geek.js'>" },
+      { text: "<script name= 'geek.js'>" },
+    ],
+    correct: "<script src= 'geek.js'>",
+  },
+  {
+    question:
+      "Which of the following is the correct syntax to display GeeksforGeeks in an alert box using JavaScript?",
+    answers: [
+      { text: "alertbox('GeeksforGeeks')" },
+      { text: "msg ('GeeksforGeeks')" },
+      { text: "msgbox('GeeksforGeeks')" },
+      { text: "alert('GeeksforGeeks')" },
+    ],
+    correct: "alert('GeeksforGeeks')",
+  },
+  {
+    question: "Which of the following is ot reserved word in JavaScript?",
+    answer: [
+      { text: "interface" },
+      { text: "throws" },
+      { text: "program" },
+      { text: "short" },
+    ],
+    correct: "program",
+  },
+  {
+    question:
+      "What is the HTML tag under which one can write the JavaScript code?",
+    answer: [
+      { text: "<javascript>" },
+      { text: "<scripted>" },
+      { text: "<script>" },
+      { text: "<js>" },
+    ],
+    correct: "<script>",
+  },
+  {
+    question:
+      "Which of the following function of string object combines the text of two strings and returns a new string?",
+    answer: [
+      { text: "concat()" },
+      { text: "add()" },
+      { text: "merge()" },
+      { text: "append" },
+    ],
+    correct: "concat()",
+  },
+  {
+    question: "Which operator is used to assign a value to a variable?",
+    answer: [{ text: "-" }, { text: "X" }, { text: "*" }, { text: "=" }],
+    correct: "=",
+  },
+  {
+    question: "How to add comments in JavaScript?",
+    answer: [
+      { text: "//This is a comment" },
+      { text: "<!--This is a comment-->" },
+      { text: "/*This is a comment*/" },
+      { text: "-This is a comment" },
+    ],
+    correct: "//This is a comment",
+  },
+  {
+    question:
+      "What is the syntax for creating a function in JavaSCript named booFunction?",
+    answer: [
+      { text: "function: = booFunction()" },
+      { text: "function+ booFunction()" },
+      { text: "function = booFunction()" },
+      { text: "function booFunction()" },
+    ],
+    correct: "function booFunction()",
+  },
+  {
+    question: "How to call a function in JavaScript?",
+    answer: [
+      { text: "booFunction();" },
+      { text: "function call booFunction();" },
+      { text: "Function();" },
+      { text: "function booFunction()" },
+    ],
+    correct: "booFunction();",
+  },
+];
